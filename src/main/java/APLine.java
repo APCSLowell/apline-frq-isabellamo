6e4fbc7c9d8dd77cbb51458ab9f5bf26f53729ba
isabellamo <142440827+isabellamo@users.noreply.github.com>
2024-04-29 23:57:42 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,20 +1,17 @@
 public class APLine {
   private int a, b, c;
 
-    public APLine(int a, int b, int c)
-    {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public APLine(int aIn, int bIn, int cIn){
+        this.a = aIn;
+        this.b = bIn;
+        this.c = cIn;
     }
 
-    public double getSlope()
-    {
-        return -(a / ((double) b));
+    public double getSlope(){
+        return - (a / ((double) b));
     }
 
-    public boolean isOnLine(int x, int y)
-    {
-        return a * x + b * y + c == 0;
+    public boolean isOnLine(int x, int y){
+        return (a * x) + (b * y) + c == 0;
     }
 }
